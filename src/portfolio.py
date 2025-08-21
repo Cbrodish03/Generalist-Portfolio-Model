e264e1e8602323f07aa0f679c162176c2cc224ef
Cameron Kooken <cbkooken@gmail.com>
2025-08-21 13:15:20 -0400

    Fixed Markowitz bullet & added custom portfolio generation

--- src/portfolio.py
+++ src/portfolio.py
@@ -15,6 +15,7 @@ class Portfolio:
          self.port['weights'] = self.rand_weights(source)
          port_type = 'rand'
       elif type == 'e':
+         # TODO: add weights calculation for Pareto-efficient port using 10% OM
          self.port['weights'] = self.eff_weights(source)
          port_type = 'eff'
       else:
@@ -40,17 +41,47 @@ class Portfolio:
       # Split the avg_return & variance calculations into two processes since it kept throwing a runtime error :/
       mean = np.mean(new_trials)
       var = np.var(new_trials)
+      percentile = np.percentile(new_trials, 10)
          
       self.port['metadata'] = {
          'PortType': port_type,
          'ExpectedRevenue': expected_rev,
          'AverageReturn': mean * expected_rev,
-         'Variance': var * expected_rev
+         'Variance': var * expected_rev,
+         'TenPercentOM': percentile * expected_rev
       }
 
       self.port['trials'] = new_trials
 
       return self.port
+   
+   def cust_weights(self, source):
+      count = len(source)
+      # Default: all assets weighted uniformly
+      weights = np.full(count, 1/count)
+
+      # I built this system like an RPG character stat builder, there's absolutely an easier/more UI-friendly way to do this
+      # The theory on this is that every 10 "port points" represents 1% of the total custom portfolio build - i.e. if an asset is assigned 400 port points, it represents 40% of the portfolio by volume 
+      port_points = 1000
+      for i in range(count):
+         asset_points = int(input(f"Weight for Asset {i + 1}:\n"))
+         port_points -= asset_points
+         if (asset_points < 0) or (port_points < 0):
+            raise ValueError("Invalid asset weight!")
+         print(f"Port points remaining: {port_points}\n")
+         weights[i] = asset_points/1000
+
+         for j in range(i + 1, count):
+            weights[j] = (1000 - port_points)/(count * 1000)
+
+      if port_points > 0:
+         # TODO: add user prompt re: normalizing asset weights
+         for i in range(count):
+            old = weights[i]
+            weights[i] = (old * 1000)/(1000 - port_points)
+
+      return weights
+
    
    def rand_weights(self, source):
       # TODO: give users more control over alpha values => control "degree of randomness"
@@ -106,20 +137,25 @@ if __name__ == "__main__":
    # SLURP = SIPParser("data/small_SIP.xlsx")
    group_data = SLURP.investments
 
+   # Test for custom case:
+   gen = Portfolio()
+   cust = gen.construct_port(group_data)
+   print(cust['metadata']['AverageReturn'], cust['metadata']['Variance'], cust['weights'], sep=', ')
+   
    # Test for random case:
-   sample_portfolios = []
+   """ sample_portfolios = []
    for i in range(10):
       gen = Portfolio()
       rand = gen.construct_port(group_data, 'r')
       sample_portfolios.append(rand)
 
    for port in sample_portfolios:
-      print(port['metadata']['AverageReturn'], port['metadata']['Variance'], port['weights'], sep=', ')
+      print(port['metadata']['AverageReturn'], port['metadata']['Variance'], port['weights'], sep=', ') """
 
    # Test for efficient case:
-   gen = Portfolio()
+   """ gen = Portfolio()
    eff = gen.construct_port(group_data, 'e')
-   print(eff['metadata']['AverageReturn'], eff['metadata']['Variance'], eff['weights'], sep=', ')
+   print(eff['metadata']['AverageReturn'], eff['metadata']['Variance'], eff['weights'], sep=', ') """
    
 
 
